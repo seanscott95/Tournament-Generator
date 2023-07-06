f3e830060a1c346344897d79ad92c86287f3a2c8
Sean <seanms418@gmail.com>
2023-07-06 16:23:05 +1000

    when the generate btn is clicked the current list of names will be matched up creating a round robin array, the array now needs to be sorted so there is an even amount of games played for people as we progress through the games, the genrator component will now display the games after the generate btn is clicked

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,7 +1,14 @@
 import useGenerator from '../hooks/useGenerator';
 
 const Generator = () => {
-    const { addTeamName, teamNameInput, setTeamNameInput, teamNames } = useGenerator();
+    const {
+        addTeamName,
+        teamNameInput,
+        setTeamNameInput,
+        teamNames,
+        handleGenerateClick,
+        allGames
+    } = useGenerator();
 
     return (
         <>
@@ -12,7 +19,12 @@ const Generator = () => {
             <section>
                 <div className="inputGroup">
                     <label htmlFor="teams">Teams</label>
-                    <input type="text" id="teams" name="teams" value={teamNameInput} onChange={(e) => setTeamNameInput(e.target.value)} />
+                    <input
+                        type="text"
+                        id="teams"
+                        name="teams"
+                        value={teamNameInput} onChange={(e) => setTeamNameInput(e.target.value)} 
+                    />
                     <button onClick={addTeamName}>Add</button>
                 </div>
             </section>
@@ -26,9 +38,15 @@ const Generator = () => {
             </section>
             <section>
                 <div className="inputGroup">
-                    <p>Click the "GENERATE" to display your randomised round robin table below</p>
-                    <button onClick={() => {}}>GENERATE</button>
+                    <p>Click the "GENERATE" to display your randomised round robin schedule below</p>
+                    <button onClick={() => handleGenerateClick()}>GENERATE</button>
                 </div>
+            </section>
+            <section>
+                {allGames && allGames.map((game) => {
+                    console.log('game', game)
+                    return <h2>{game[0]} VS {game[1]}</h2>
+                })}
             </section>
         </>
     );
--- src/hooks/useGenerator.jsx
+++ src/hooks/useGenerator.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const useGenerator = () => {
     const [teamNameInput, setTeamNameInput] = useState('');
     const [teamNames, setTeamNames] = useState([]);
+    const [allGames, setAllGames] = useState([]);
     console.log("teamNameInput", teamNameInput)
     console.log("teamNames", teamNames)
     const addTeamName = () => {
@@ -10,11 +11,60 @@ const useGenerator = () => {
         setTeamNameInput('')
     };
 
+    // have a list of strings in an array
+    // we want to grab each item and return an array of arrays
+    const handleGenerateClick = () => {
+        let tempList = [...teamNames];
+        let tempArr = [];
+        const newArr = teamNames.length >= 2 && teamNames.map((team) => {
+            tempList.map((item) => {
+                if (team === item) {
+                    return;
+                };
+                console.log('team', team);
+                const teamPair = [team, item];
+                console.log('teamPair', teamPair);
+                tempArr.push(teamPair);
+            });
+            tempList.shift();
+        });
+
+        console.log('tempArr', tempArr)
+        setAllGames(tempArr);
+    };
+    // those objects will contain the original item plus each item
+    // so if there is three names ['bob, 'pal', 'dave']
+    // we want to return
+    // [
+    //    ["bob", "pal"],
+    //    ["bob", "dave"],
+    //    ["pal", "dave"]
+    // ]
+    //
+    // so if there is five names ['bob, 'pal', 'dave', 'nath', 'jack']
+    // we want to return
+    // [
+    //    ["bob": "pal"],
+    //    ["bob": "dave"],
+    //    ["bob": "nath"],
+    //    ["bob": "jack"],
+    //    ["pal": "dave"],
+    //    ["pal": "nath"],
+    //    ["pal": "jack"],
+    //    ["dave": "nath"],
+    //    ["dave": "jack"],
+    //    ["nath": "jack"]
+    // ]
+    //
+    //
+
     return {
         addTeamName,
         teamNameInput,
         setTeamNameInput,
-        teamNames
+        teamNames,
+        handleGenerateClick,
+        allGames
     };
 };
 
