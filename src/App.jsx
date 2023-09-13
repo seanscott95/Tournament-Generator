330139273988262cea7f7dc3876faa0af9ae20f9
Sean <seanms418@gmail.com>
2023-09-13 18:28:00 +1000

    DELETED layout folder and files. Installed react router dom and setup in the app component. Created a navbar that has styled buttons for each type of tournament. Created media query for navbar buttons to display in a column under screen size of 450px. Created a homepage component that displays nothing at the moment. Component has been used in the app component as the home route for react router. Routes for each tournament type display the round robin generator component for development purposes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import TournamentLayout from './layouts/TournamentLayout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import RRGenerator from './components/RRGenerator';
 
 const App = () => {
   return (
     <>
-      <TournamentLayout />      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/roundRobin" element={<RRGenerator />} />
+          <Route path="/single" element={<RRGenerator />} />
+          <Route path="/double" element={<RRGenerator />} />
+        </Routes>
+        <p className="m">Margin</p>
+      </Router>
     </>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <Link to='/' className='heading'>
+        <h1 >TOURNAMENT GENERATOR</h1>
+      </Link>
+      <ul>
+        <li>
+          <Link to='roundRobin'>
+            <button className='btnNeon customBtn'>Round Robin</button>
+          </Link>
+        </li>
+        <li>
+          <Link to='roundRobin'>
+            <button className='btnNeon customBtn'>Single</button>
+          </Link>
+        </li>
+        <li>
+          <Link to='roundRobin'>
+            <button className='btnNeon customBtn'>Double</button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
