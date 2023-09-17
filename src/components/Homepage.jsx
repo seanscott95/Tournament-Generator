3d3c82d8dbb12575f26cf4eff3e3f0682a1f8c43
Sean <seanms418@gmail.com>
2023-09-17 16:37:50 +1000

    Navbar, removed an unused import for an fa icon. Homepage, created homepage that includes a header, list of dotpoints about the app and a photo of the app. Homepage is now styled and responsive.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPen, faUserGroup, faUserCheck, faPersonChalkboard } from '@fortawesome/free-solid-svg-icons';
+
+import RRMatches from '../assets/images/roundRobinMatches.PNG';
+
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <section className='homepage'>
+        <h1>Create tournaments with your friends and see who really is the best...</h1>
+        <div className='infoContainer'>
+          <ul>
+            <li>
+              <FontAwesomeIcon className="icon" icon={faUserPen} />
+              Choose from a Round Robin, Single and Double Elimination Tournament</li>
+            <li>
+              <FontAwesomeIcon className="icon" icon={faPersonChalkboard} />
+              Organise your tournaments with this simple to use generator</li>
+            <li>
+              <FontAwesomeIcon className="icon" icon={faUserCheck} />
+              No subscription required, free and easy to use</li>
+            <li>
+              <FontAwesomeIcon className="icon" icon={faUserGroup} />
+              No limit for number of teams
+            </li>
+          </ul>
+          <img src={RRMatches} alt="Generated tournament" />
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrophy, faUserGroup } from '@fortawesome/free-solid-svg-icons';
+import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
