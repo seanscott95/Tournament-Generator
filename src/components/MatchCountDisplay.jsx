b4702108eba652b1f3b262ebcae1a87cb86c0622
Sean <seanms418@gmail.com>
2023-10-16 18:25:32 +1100

    Created a title that shows the round of the tournament. Styled and positioned the title next to the list of icons for each match.

--- src/components/MatchCountDisplay.jsx
+++ src/components/MatchCountDisplay.jsx
@@ -7,6 +7,7 @@ const MatchCountDisplay = ({ allGamesObj }) => {
   console.log('allGO', allGamesObj);
   let length = allGamesObj.length;
   const [count, setCount] = useState(0);
+  const [round, setRound] = useState('');
 
   useEffect(() => {
     let c = 0;
@@ -20,26 +21,35 @@ const MatchCountDisplay = ({ allGamesObj }) => {
     console.log('c2', c);
     setCount(c);
     console.log('count', count);
+    // Updates the round state with the round value in local storage
+    const roundFromLocal = JSON.parse(localStorage.getItem('round')) || '';
+    if (roundFromLocal !== null) setRound(roundFromLocal);
   }, [allGamesObj]);
+  
   return (
-    <div className="matchFinishedIcon">
-      {allGamesObj &&
-        allGamesObj.map((game) => {
-          return (
-            <>
-              {game.completed ? (
-                <p className="completed">
-                  <FontAwesomeIcon className="icon" icon={faUserGroup} />
-                </p>
-              ) : (
-                <p className="notCompleted">
-                  <FontAwesomeIcon className="icon" icon={faUserGroup} />
-                </p>
-              )}
-            </>
-          );
-        })}
-      {allGamesObj && <p>{`${count}/${length}`}</p>}
+    <div className="matchCountInformation">
+      <div className="roundTitle">
+        <h1>Round {round}</h1>
+      </div>
+      <div className="matchFinishedIcon">
+        {allGamesObj &&
+          allGamesObj.map((game, index) => {
+            return (
+              <div key={game + index}>
+                {game.completed ? (
+                  <p className="completed">
+                    <FontAwesomeIcon className="icon" icon={faUserGroup} />
+                  </p>
+                ) : (
+                  <p className="notCompleted">
+                    <FontAwesomeIcon className="icon" icon={faUserGroup} />
+                  </p>
+                )}
+              </div>
+            );
+          })}
+        {allGamesObj && <p>{`${count}/${length}`}</p>}
+      </div>
     </div>
   );
 };
